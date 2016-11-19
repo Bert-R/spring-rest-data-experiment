eeaba69c8f769579b552972a3365e266b67dd33d
Bert Roos <Bert-R@users.noreply.github.com>
2016-11-19 18:03:34 +0100

    Corrected the content type header
    The hyphen in the ``Content-Type`` header for the request that establishes the relationship between the entities was missing.

--- loadData.bat
+++ loadData.bat
@@ -1,3 +1,3 @@
 curl -i -X POST -H "Content-Type:application/json" -d "{  \"length\" : \"30\" }" http://localhost:8080/shelves
 curl -i -X POST -H "Content-Type:application/json" -d "{  \"title\" : \"The Battle of Life\" }" http://localhost:8080/books
-curl -i -X PUT -H "ContentType: text/uri-list" -d "http://localhost:8080/shelves/1" http://localhost:8080/books/1/shelve
+curl -i -X PUT -H "Content-Type: text/uri-list" -d "http://localhost:8080/shelves/1" http://localhost:8080/books/1/shelve
